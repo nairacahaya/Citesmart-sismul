8bd9b7c1b8882e98ee30690e02ec0faa0ae0efc4
nairacahaya <borunaga06@gmail.com>
2025-12-27 11:59:09 +0700

    Create data.js

---
+++ data.js
@@ -0,0 +1,34 @@
+const booksData = [
+    {
+        id: 1,
+        title: "Sistem Informasi Manajemen",
+        author: "Dr. Ir. H. A. Abdul Aziz",
+        category: "Buku",
+        image: "https://images.unsplash.com/photo-1544716278-ca5e3f4abd8c?q=80&w=400",
+        tag: "Terbaru"
+    },
+    {
+        id: 2,
+        title: "Jurnal Teknologi Informasi Vol. 5",
+        author: "Telkom University",
+        category: "Jurnal",
+        image: "https://images.unsplash.com/photo-1532152294817-63519998b964?q=80&w=400",
+        tag: "Populer"
+    },
+    {
+        id: 3,
+        title: "Algoritma dan Pemrograman",
+        author: "Suharto S.Kom",
+        category: "E-Book",
+        image: "https://images.unsplash.com/photo-1515879218367-8466d910aaa4?q=80&w=400",
+        tag: "E-Book"
+    },
+    {
+        id: 4,
+        title: "Analisis Sentimen Twitter",
+        author: "Andi Wijaya",
+        category: "Skripsi",
+        image: "https://images.unsplash.com/photo-1456513080510-7bf3a84b82f8?q=80&w=400",
+        tag: "Tugas Akhir"
+    }
+];
